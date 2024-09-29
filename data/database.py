3475896e1efedb54586bd25ac93d9b9969d3dbf0
Hayk <haykooo3@gmail.com>
2024-09-29 17:45:32 +0300

    database.py update

--- data/database.py
+++ data/database.py
@@ -5,109 +5,93 @@ from data.random_parson import get_person
 # Создание соединения с базой данных и создание таблицы, если она не существует
 def db_start():
     # Устанавливаем соединение с базой данных
-    connection = sq.connect('user.db')
-    cur = connection.cursor()
+    with sq.connect('user.db') as db:
+        cur = db.cursor()
+        # Создаем таблицу
+        cur.execute("""CREATE TABLE IF NOT EXISTS user (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER,
+                        name TEXT,
+                        fname TEXT,
+                        age INTEGER,
+                        phone TEXT,
+                        job_title TEXT,
+                        gender TEXT)""")
 
-    # Создаем таблицу
-    cur.execute("""CREATE TABLE IF NOT EXISTS user (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_id INTEGER,
-                    name TEXT,
-                    fname TEXT,
-                    age INTEGER,
-                    phone TEXT,
-                    job_title TEXT,
-                    gender TEXT)""")
-
-    # Сохраняем изменения и закрываем соединение
-    connection.commit()
-    connection.close()
+        # Сохраняем изменения
+        db.commit()
 
 
+# Вставка записи в таблицу user
 def db_insert_random(num=100):
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
 
-    for i in range(1, num+1):
-        user_id = i
-        name, fname, age, phone, job_title, gender = get_person()
-        cur.execute(
-            f"INSERT INTO user (user_id, name, fname, age, phone, job_title, gender) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (user_id, name, fname, age, phone, job_title, gender))
+        for i in range(1, num + 1):
+            user_id = i
+            name, fname, age, phone, job_title, gender = get_person()
+            cur.execute(
+                f"INSERT INTO user (user_id, name, fname, age, phone, job_title, gender) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (user_id, name, fname, age, phone, job_title, gender))
 
-    connection.commit()
-    connection.close()
-
-        # Вставка записи в таблицу
+        db.commit()
 
 
 def db_insert(user_id, name, fname, age, phone, job_title, gender):
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
 
-    # Проверяем, существует ли уже запись с таким же названием
-    if not cur.execute("SELECT * FROM user WHERE user_id = ?", (user_id,)).fetchone():
-        cur.execute(
-            f"INSERT INTO user (user_id, name, fname, age, phone, job_title, gender) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (user_id, name, fname, age, phone, job_title, gender))
+        # Проверяем, существует ли уже запись с таким же названием
+        if not cur.execute("SELECT * FROM user WHERE user_id = ?", (user_id,)).fetchone():
+            cur.execute(
+                f"INSERT INTO user (user_id, name, fname, age, phone, job_title, gender) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (user_id, name, fname, age, phone, job_title, gender))
 
-    connection.commit()
-    connection.close()
+        db.commit()
 
 
 # Обновление записи в таблице
 def db_update(user_id, name, fname, age, phone, job_title, gender):
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
 
-    # Обновляем запись с указанным названием
-    cur.execute("UPDATE user SET name = ?, fname = ?, age = ?, phone = ?, job_title = ?, gender = ? WHERE user_id = ?",
-                (name, fname, age, phone, job_title, gender, user_id))
+        # Обновляем запись с указанным названием
+        cur.execute(
+            "UPDATE user SET name = ?, fname = ?, age = ?, phone = ?, job_title = ?, gender = ? WHERE user_id = ?",
+            (name, fname, age, phone, job_title, gender, user_id))
 
-    connection.commit()
-    connection.close()
+        db.commit()
 
 
 # Выбор всех записей из таблицы
 def db_select():
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
-
-    users = cur.execute("SELECT * FROM user").fetchall()
-
-    connection.close()
-    return users
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
+        users = cur.execute("SELECT * FROM user").fetchall()
+        return users
 
 
 # Удаление записи из таблицы по названию
 def db_delete(user_id):
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
-
-    cur.execute("DELETE FROM user WHERE user_id = ?", (user_id,))
-
-    connection.commit()
-    connection.close()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
+        cur.execute("DELETE FROM user WHERE user_id = ?", (user_id,))
+        db.commit()
 
 
 def id_exist(user_id):
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
-
-    return not cur.execute("SELECT * FROM user WHERE user_id = ?", (user_id,)).fetchone()
-
-    connection.commit()
-    connection.close()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
+        return not cur.execute("SELECT * FROM user WHERE user_id = ?", (user_id,)).fetchone()
 
 
 def db_delete_all():
-    connection = sq.connect('data/user.db')
-    cur = connection.cursor()
+    with sq.connect('data/user.db') as db:
+        cur = db.cursor()
+        cur.execute("DELETE FROM user")
+        db.commit()
 
-    cur.execute("DELETE FROM user")
 
-    connection.commit()
-    connection.close()
 
 # Тестовый код
 if __name__ == '__main__':
